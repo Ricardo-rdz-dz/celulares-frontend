96bfb62f6ea5b8b7b66146feb3d3acb2de5908cd
Ricardo <drums417@gmail.com>
2026-05-18 15:31:51 -0700

    se modifican detalles visuales ne los botones

--- app/admin/ticket/[id]/page.tsx
+++ app/admin/ticket/[id]/page.tsx
@@ -142,13 +142,8 @@ export default function TicketDetail() {
                   <p className="text-xl font-bold text-slate-800">{ticket.clientes.nombre}</p>
                   <p className="text-slate-500 font-medium mt-1"> {ticket.clientes.telefono}</p>
                 </div>
-                <button 
-  type="button"
-  onClick={abrirWhatsApp} 
-  className="bg-green-100 text-green-700 px-4 py-2 rounded-lg font-bold hover:bg-green-200 transition"
->
-  Abrir WhatsApp
-</button>
+
+                
    <div className="flex gap-2">
   {/* Botón Azul: Notificación de estatus normal */}
   <button 
